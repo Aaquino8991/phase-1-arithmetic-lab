017ffbd8d56ad7f76cfb2b5750ccf19f797899a9
Anthony <aquinoanthony851@gmail.com>
2023-04-07 19:03:31 -0600

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let min = 1;
+let maximum = 10;
+
+let random = Math.ceil(Math.random() * (maximum - min) + min);
+
+Number.isInteger(random);
+
+const num3 = 27;
+const num4 = 23;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 4, 6, 20);
